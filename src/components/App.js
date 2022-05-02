f56363fb639d5db7865f729016542c55dd9a4ffb
Naiara Saratxaga <99652446+NaiSaratxaga@users.noreply.github.com>
2022-05-02 22:38:03 +0200

    Local Storage

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,40 @@
-import "../styles/App.scss";
-import logoAwesome from "../images/awesome.svg";
-import logo from "../images/logo_ninfas.png";
-import { useState } from "react";
-import dataApi from "../services/Api";
-import defaultAvatar from "../images/mariposa.jpg";
+import '../styles/App.scss';
+import logoAwesome from '../images/awesome.svg';
+import logo from '../images/logo_ninfas.png';
+import { useState } from 'react';
+import dataApi from '../services/Api';
+import localStorage from '../services/localStorage';
+import defaultAvatar from '../images/mariposa.jpg';
+import Cards from './Cards';
 
-import Cards from "./Cards";
+//Aquí calculamos el estado inicial DataCard, si se ha guardado previamente en LS se usa ese valor. Sino se crea uno por defecto.
+function getInitialDataCardState() {
+  const dataCardFromLocalStorage = localStorage.get('dataCard');
+  return (
+    dataCardFromLocalStorage || {
+      palette: '1',
+      name: '',
+      job: '',
+      email: '',
+      phone: '',
+      linkedin: '',
+      github: '',
+    }
+  );
+}
+
+function getInitialAvatarState() {
+  const avatarFromLocalStorage = localStorage.get('avatar');
+  return avatarFromLocalStorage || defaultAvatar;
+}
+
+//Al crear estado dataCard le pasamos valor por defecto que calcula getInitialDataCardState.
+//He quitado la img de dataCard.
+//Al crear estado avatar le pasamos valor por defecto que calcula getInitialAvatarState.(cada uno tiene su state y cada uno se guarda en LS con una clave distinta)
 
 function App() {
-  const [dataCard, setDataCard] = useState({
-    palette: "1",
-    name: "",
-    job: "",
-    photo: "../images/mariposa.jpg",
-    email: "",
-    phone: "",
-    linkedin: "",
-    github: "",
-  });
-  const [avatar, setAvatar] = useState("");
+  const [dataCard, setDataCard] = useState(getInitialDataCardState());
+  const [avatar, setAvatar] = useState(getInitialAvatarState());
 
   const [apiData, setApiData] = useState({});
 
@@ -32,25 +48,30 @@ function App() {
   };
   const updateAvatar = (avatar) => {
     setAvatar(avatar);
+    localStorage.set('avatar', avatar);
   };
 
   const handleInputChange = (inputValue, inputChanged) => {
-    setDataCard({
+    const newDataCard = {
       ...dataCard,
       [inputChanged]: inputValue,
-    });
+    };
+    setDataCard(newDataCard);
+    localStorage.set('dataCard', newDataCard);
   };
+
+  //Cuando se haga reset, también se borrará LS.
   const handleReset = (ev) => {
     ev.preventDefault();
+    localStorage.clear();
     setDataCard({
-      palette: "1",
-      name: "",
-      job: "",
-      image: "../images/mariposa.jpg",
-      email: "",
-      phone: "",
-      linkedin: "",
-      github: "",
+      palette: '1',
+      name: '',
+      job: '',
+      email: '',
+      phone: '',
+      linkedin: '',
+      github: '',
     });
     setAvatar(defaultAvatar);
   };
